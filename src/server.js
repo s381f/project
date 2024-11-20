302f7597d3c8d53426ce38b1557a7f0fe9a6ab27
Leo Wu <leo.wu.kw@gmail.com>
2024-11-20 15:19:59 +0800

    fix login error "True is not defined"

--- src/server.js
+++ src/server.js
@@ -76,7 +76,7 @@ app.post('/login',async(req,res) => {
   const{username, password} = req.body;
   const user = await User.findOne({username});
   const isPasswordvalid =await bcrypt.compare(password, user.password);
-  if(user == True && isPasswordvalid == True){
+  if(user && isPasswordvalid == true){
     return res.redirect('/dashboard');
   }else{
     return res.render("message",{message: 'Invalid username or password'});
